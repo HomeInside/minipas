64196af8c71ab02fe45069ffaef5e146eb137117
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-09-25 12:35:09 -0500

    [in progress] functions to generate Pairs and AST separately new functions gen_pairs and gen_ast

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -14,3 +14,22 @@ pub mod program;
 pub mod statements;
 pub mod symbol_table;
 pub mod types;
+use pest::Parser;
+use pest::iterators::Pairs;
+
+use crate::MiniPasParser;
+use crate::Rule;
+use crate::parser::ast::Stmt;
+use crate::parser::program::parse_program;
+use crate::parser::symbol_table::SymbolTable;
+
+pub fn gen_pairs(source: &str) -> Pairs<'_, Rule> {
+    MiniPasParser::parse(Rule::program, source).unwrap_or_else(|e| panic!("Error de parseo: {} {}", source, e))
+}
+
+/// Genera el AST completo a partir del código fuente (para `run`, `check`, `build`)
+pub fn gen_ast(source: &str) -> (Vec<Stmt>, SymbolTable) {
+    let pairs = gen_pairs(source);
+    //let (program, _) = parse_program(pairs);
+    parse_program(pairs)
+}
