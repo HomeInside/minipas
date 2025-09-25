7160df11df4b73a30a39f3c3be6d64d6df6d6704
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-09-25 14:09:43 -0500

    new functions: read_ast_from_bincode, save_pairs_as_text and save_ast_as_text to generate files from emit command (pairs and ast) as well load binary from file

--- src/cli/binary.rs
+++ src/cli/binary.rs
@@ -5,16 +5,34 @@ use std::path::PathBuf;
 #[derive(Encode, Decode, Debug)]
 pub struct BinAstFile(Vec<Stmt>);
 
+use pest::iterators::Pairs;
+
+use crate::Rule;
+
 pub fn gen_bincode_from_ast(code: Vec<Stmt>, output: &PathBuf) {
     let config = config::standard();
 
     let program = BinAstFile(code);
-    //println!("program: {:#?}", program);
 
     let encoded: Vec<u8> = bincode::encode_to_vec(program, config).expect("Error serializando AST");
-    //println!("encoded: {:?}", encoded);
-    //println!("output: {:?}", output);
 
     std::fs::write(output, encoded).expect("Error escribiendo binario");
-    println!("Generado binario {:?}", output);
+}
+
+pub fn read_ast_from_bincode(input: &PathBuf) -> Vec<Stmt> {
+    let bytes = std::fs::read(input).expect("No se pudo leer el archivo .mpc");
+    let config = bincode::config::standard();
+
+    let (program, _): (BinAstFile, usize) =
+        bincode::decode_from_slice(&bytes, config).expect("Error decodificando binario");
+
+    program.0
+}
+
+pub fn save_pairs_as_text(pairs: &Pairs<Rule>, output: &PathBuf) {
+    std::fs::write(output, format!("{:#?}", pairs)).expect("No se pudo escribir archivo .mpp");
+}
+
+pub fn save_ast_as_text(code: Vec<Stmt>, output: &PathBuf) {
+    std::fs::write(output, format!("{:#?}", code)).expect("No se pudo escribir archivo .mpa");
 }
