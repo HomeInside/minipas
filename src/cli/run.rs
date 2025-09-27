876ffdb0759766f96d5b981b7c6edf7207c2c323
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-09-27 10:09:18 -0500

    added SymbolTable::with_builtins to generate symbo table to run program

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -2,6 +2,7 @@ use super::binary::read_ast_from_bincode;
 use super::core::read_source;
 use crate::parser::ast::Stmt;
 use crate::parser::gen_ast;
+use crate::parser::symbol_table::SymbolTable;
 use crate::print_info;
 use crate::runtime::{
     interpreter::{RuntimeEnv, execute_stmt},
@@ -24,11 +25,14 @@ pub fn run_cmd(input: Option<PathBuf>) {
     if let Some(ext) = input.extension() {
         let ext_str = ext.to_string_lossy().to_lowercase();
 
+        let mut sym_table: SymbolTable = SymbolTable::with_builtins();
+
         let ast: Vec<Stmt> = match ext_str.as_str() {
             "mp" => {
                 println!("listo para parsear y ejecutar: {:?}", input.display());
                 let src = read_source(&input);
-                let (ast, _) = gen_ast(&src);
+                let (ast, sym_tbl) = gen_ast(&src);
+                sym_table = sym_tbl;
                 ast
             }
             "mpc" => {
@@ -45,6 +49,10 @@ pub fn run_cmd(input: Option<PathBuf>) {
 
         // Crear el entorno y ejecutar el programa
         let mut env = RuntimeEnv::new();
+        // Cargar la tabla de simbolos
+        for (name, ty) in sym_table.global_vars() {
+            env.declare(&name, ty);
+        }
 
         for stmt in &ast {
             if let Err(e) = execute_stmt(stmt, &mut env, &BUILTINS) {
