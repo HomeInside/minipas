2396e7397cc83ce5fe60446ec23ccdab90f7aad1
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-09-27 10:13:09 -0500

    fix validate symbol table on vars block definitions (funcs and procs) :fire:

--- src/parser/declarations.rs
+++ src/parser/declarations.rs
@@ -12,17 +12,40 @@ pub fn parse_var_section(pair: Option<Pair<Rule>>, sym_table: &mut SymbolTable) 
         for var_decl_pair in var_section_pair.into_inner() {
             if var_decl_pair.as_rule() == Rule::var_decl {
                 let mut inner = var_decl_pair.into_inner();
-                let ids_pair = inner.next().unwrap(); // ident list
-                let ty_pair = inner.next().unwrap(); // type
 
-                let ty = parse_type(ty_pair);
-                for id_pair in ids_pair.into_inner() {
-                    let id = id_pair.as_str().to_string();
-                    locals.push(Param {
-                        name: id.clone(),
-                        ty: ty.clone(),
-                    });
-                    sym_table.insert(id, ty.clone());
+                let mut ids = Vec::new();
+                let mut ty_pair_opt = None;
+
+                while let Some(next) = inner.peek() {
+                    match next.as_rule() {
+                        Rule::ident => {
+                            ids.push(inner.next().unwrap());
+                        }
+                        Rule::comma => {
+                            inner.next(); // ignorar coma
+                        }
+                        Rule::keyword_integer | Rule::keyword_real | Rule::keyword_string | Rule::keyword_boolean => {
+                            ty_pair_opt = Some(inner.next().unwrap());
+                            break;
+                        }
+                        _ => {
+                            break;
+                        }
+                    }
+                }
+
+                if let Some(ty_pair) = ty_pair_opt {
+                    let ty = parse_type(ty_pair);
+
+                    for id_pair in ids {
+                        let id = id_pair.as_str().to_string();
+
+                        locals.push(Param {
+                            name: id.clone(),
+                            ty: ty.clone(),
+                        });
+                        sym_table.insert(id, ty.clone());
+                    }
                 }
             }
         }
