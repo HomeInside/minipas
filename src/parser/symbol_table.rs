9accca5fcf3fbcfbd5106f713416d56db35575eb
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-09-27 10:10:36 -0500

    added global_vars and with_builtins methods to handle symbol tables and vars
    added nil as symbol

--- src/parser/symbol_table.rs
+++ src/parser/symbol_table.rs
@@ -1,7 +1,7 @@
 use crate::parser::ast::VarType;
 use std::collections::HashMap;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct SymbolTable {
     variables: HashMap<String, VarType>,
 }
@@ -12,10 +12,26 @@ impl SymbolTable {
             variables: HashMap::new(),
         }
     }
+    pub fn global_vars(&self) -> Vec<(String, VarType)> {
+        self.variables
+            .iter()
+            .map(|(name, ty)| (name.clone(), ty.clone()))
+            .collect()
+    }
+
+    pub fn with_builtins() -> Self {
+        let mut table = SymbolTable::new();
+
+        // Constante nula
+        table.insert("nil".to_string(), VarType::Nil);
+
+        table
+    }
     pub fn declare(&mut self, name: &str, typ: VarType) -> Result<(), String> {
         if self.variables.contains_key(name) {
             return Err(format!("Variable '{}' ya declarada", name));
         }
+
         self.variables.insert(name.to_string(), typ);
         Ok(())
     }
