c73e4456b9f29ab2109e9f5dbd5aa371403eba07
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-09-29 18:12:42 -0500

    rework assert_eq_fn, now validate each data type :fire:

--- src/runtime/std_lib/testing.rs
+++ src/runtime/std_lib/testing.rs
@@ -19,8 +19,31 @@ pub fn assert_eq_fn(args: Vec<Value>) {
         panic!("assert_eq() necesita exactamente 2 argumentos");
     }
 
-    if args[0] != args[1] {
-        panic!("Assertion failed: expected `{}`, got `{}`\n", args[0], args[1]);
+    let a = &args[0];
+    let b = &args[1];
+
+    let resp = match (a, b) {
+        // mismos tipos
+        (Value::Integer(x), Value::Integer(y)) => x == y,
+        (Value::Real(x), Value::Real(y)) => (x - y).abs() < f64::EPSILON,
+        (Value::Str(x), Value::Str(y)) => x == y,
+        (Value::Boolean(x), Value::Boolean(y)) => x == y,
+        (Value::Byte(x), Value::Byte(y)) => x == y,
+        (Value::Nil, Value::Nil) => true,
+
+        // validación entre tipos numéricos
+        (Value::Integer(x), Value::Byte(y)) => *x == *y as i64,
+        (Value::Byte(x), Value::Integer(y)) => *x as i64 == *y,
+        (Value::Integer(x), Value::Real(y)) => (*x as f64 - *y).abs() < f64::EPSILON,
+        (Value::Real(x), Value::Integer(y)) => (*x - *y as f64).abs() < f64::EPSILON,
+        (Value::Byte(x), Value::Real(y)) => (*x as f64 - *y).abs() < f64::EPSILON,
+        (Value::Real(x), Value::Byte(y)) => (*x - *y as f64).abs() < f64::EPSILON,
+
+        _ => false,
+    };
+
+    if !resp {
+        panic!("Assertion failed: expected left: `{}`, got right: `{}`", a, b);
     }
 }
 
