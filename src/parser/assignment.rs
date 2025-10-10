6ab8c6e71aa385ec6c821036c9be56be5a3a2775
Jorge Brunal Pérez <jorge.brunal@gmail.com>
2025-10-10 18:16:32 -0500

    added abbreviated operations :new:
    added `+=`, `-=`, `*=`, `/=` and `%=`

--- src/parser/assignment.rs
+++ src/parser/assignment.rs
@@ -1,4 +1,4 @@
-use super::ast::Stmt;
+use super::ast::{Expr, Op, Stmt};
 use super::expressions::parse_expr;
 use super::program::validate_identifier;
 use super::symbol_table::SymbolTable;
@@ -6,14 +6,57 @@ use crate::Rule;
 use pest::iterators::Pair;
 
 pub fn parse_assignment(pair: Pair<Rule>, sym_table: &SymbolTable) -> Stmt {
-    let mut inner = pair.into_inner(); // ident ~ ":=" ~ expr ~ ";"
+    let mut inner = pair.into_inner();
     let ident = inner.next().unwrap().as_str().to_string();
 
     validate_identifier(&ident);
 
-    inner.next(); // skip ":="
+    let op_pair = inner.next().unwrap();
+    let expr = parse_expr(inner.next().unwrap(), sym_table);
+    inner.next(); // skip ";"
 
-    let expr_pair = inner.next().unwrap();
-    let expr = parse_expr(expr_pair, sym_table);
-    Stmt::Assign(ident, expr)
+    match op_pair.as_rule() {
+        Rule::assign_op => Stmt::Assign(ident, expr),
+        Rule::assign_add => {
+            let rhs = Expr::BinaryOp {
+                left: Box::new(Expr::Ident(ident.clone())),
+                op: Op::Add,
+                right: Box::new(expr),
+            };
+            Stmt::Assign(ident, rhs)
+        }
+        Rule::assign_sub => {
+            let rhs = Expr::BinaryOp {
+                left: Box::new(Expr::Ident(ident.clone())),
+                op: Op::Sub,
+                right: Box::new(expr),
+            };
+            Stmt::Assign(ident, rhs)
+        }
+        Rule::assign_mul => {
+            let rhs = Expr::BinaryOp {
+                left: Box::new(Expr::Ident(ident.clone())),
+                op: Op::Mul,
+                right: Box::new(expr),
+            };
+            Stmt::Assign(ident, rhs)
+        }
+        Rule::assign_div => {
+            let rhs = Expr::BinaryOp {
+                left: Box::new(Expr::Ident(ident.clone())),
+                op: Op::Div,
+                right: Box::new(expr),
+            };
+            Stmt::Assign(ident, rhs)
+        }
+        Rule::assign_mod => {
+            let rhs = Expr::BinaryOp {
+                left: Box::new(Expr::Ident(ident.clone())),
+                op: Op::Mod,
+                right: Box::new(expr),
+            };
+            Stmt::Assign(ident, rhs)
+        }
+        _ => panic!("Operador de asignación desconocido"),
+    }
 }
